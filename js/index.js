f76ba215749fa15c486654309a8653b95cf92ab2
butterfly0204 <favourmogaji3@gmail.com>
2025-09-10 11:58:31 -0500

    slider added

--- js/index.js
+++ js/index.js
@@ -54,7 +54,72 @@ const hideHeader = function (){
         else{
             header.classList.remove("active");
         }
-    })
+    });
+
+
+                //========bottons===========/
+    const heroSlider = document.querySelector("[data-hero-slider]");
+    const heroSliderItems = document.querySelector("[data-hero-slider-item]");
+    const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+    const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+    let currentSlidePos =0;
+    let lastActiveSliderItem =heroSliderItems[0];
+
+    const updateSliderPos = function () {
+        lastActiveSliderItem.classList.remove("active");
+        heroSliderItems[currentSliderPos].classList.add("active");
+        lastActiveSliderItem =heroSliderItems[currentSliderPos];
+    }
+
+    const slideNext =function () {
+        if (currentSlidePos >= heroSliderItems.length -1) {
+            currentSlidePos =0;
+    }
+
+    else{
+        currentSlidePos++;
+    }
+  
+
+    updateSliderPos();
+      }
+
+      heroSliderNextBtn.addEventListener("click", slideNext);
+ 
+      const slidePrev = function (){
+        if(currentSlidePos <= 0){
+            currentSlidePos = heroSliderItems.length -1;
+        }
+
+        else{
+            currentSlidePos--;
+        }
+
+        updateSliderPos();
+      }
+
+      heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+      let autoSlideInterval;
+
+      const autoSlide =function () {
+        autoSlideInterval = setInterval(function (){
+            slideNext();
+        }, 7000);
+      }
+
+      addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function(){
+        clearInterval(autoSlideInterval);
+      });
+
+      addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide);
+
+      window.addEventListener("load", autoSlide);
+    
+     
+
+
 
 
 

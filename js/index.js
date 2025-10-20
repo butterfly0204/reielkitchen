cc99f6043ce7bc7a02d7fcf5f7387b881f9c29d2
mogaji favour <favourmogaji3@gmail.com>
2025-10-20 09:37:55 -0500

    almost don with the footer secton

--- js/index.js
+++ js/index.js
@@ -32,6 +32,7 @@ addEventOnElements(navTogglers, "click", toggleNavbar);
 //=========Header======//
 
 const header =document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 let lastScrollPos =0;
 
 const hideHeader = function (){
@@ -48,11 +49,13 @@ const hideHeader = function (){
     window.addEventListener("scroll", function (){
         if(window.scrollY >= 50){
             header.classList.add("active");
+            backTopBtn.classList.add("active");
             hideHeader();
 
         }
         else{
             header.classList.remove("active");
+            backTopBtn.classList.remove("active");
         }
     });
 
